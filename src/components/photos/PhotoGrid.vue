a845f8cbb8a5d60245362161ab37173e26f4157a
moersou <zhaoyijia2018@gmail.com>
2025-04-13 12:30:29 +0800

    chore: update photo.vue

--- src/components/photos/PhotoGrid.vue
+++ src/components/photos/PhotoGrid.vue
@@ -7,7 +7,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="photos grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4" max-w-500 mx-auto>
+  <div class="photos grid grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-4" max-w-500 mx-auto>
     <div v-for="photo, idx in photos" :key="idx">
       <img
         :src="photo.url"

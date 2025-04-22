5dc44951caa46ab420ee8cb082981287053d1814
moersou <zhaoyijia2018@gmail.com>
2025-04-22 12:53:12 +0800

    feat: update

---
+++ src/components/photos/PhotoChongQing.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Photo } from '../../../photos/data'
+import raw from '../../../photos/data'
+
+const photos: Photo[] = [
+  'p-2025-04-03-15-01-49-090-D',
+  'p-2025-04-03-15-24-18-096-D',
+  'p-2025-03-30-04-25-47-000-1',
+  'p-2025-04-06-07-19-55-000-1',
+].map(name => raw.find(p => p.url.includes(name))!)
+</script>
+
+<template>
+  <PhotoSlide :photos="photos" />
+</template>

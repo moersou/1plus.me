94c18bd821f9a8a78e06414dfec879a48439dd97
moersou <zhaoyijia2018@gmail.com>
2025-04-23 09:13:57 +0800

    feat: new photoslide

---
+++ src/components/photos/Photosample.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { Photo } from '../../../album/data'
+import raw from '../../../album/data'
+
+const photos: Photo[] = [
+  'p-2025-03-05-1',
+  'p-2025-02-09-1',
+].map(name => raw.find(p => p.url.includes(name))!)
+</script>
+
+<template>
+  <PhotoSlide :photos="photos" />
+</template>

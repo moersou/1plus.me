fea61acbea707573a783bc275fcab66f4500427e
moersou <zhaoyijia2018@gmail.com>
2026-02-12 00:56:36 +0800

    style: adjust photogrid

--- src/components/photos/PhotoGrid.vue
+++ src/components/photos/PhotoGrid.vue
@@ -7,14 +7,14 @@ defineProps<{
 </script>
 
 <template>
-  <div class="photos grid grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-4" max-w-500 mx-auto>
-    <div v-for="photo, idx in photos" :key="idx">
+  <div class="photos grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4" max-w-500 mx-auto>
+    <div v-for="photo, idx in photos" :key="idx" class="flex items-center justify-center">
       <img
         :src="photo.url"
-        :alt="photo.text"
+        :alt="photo.text || photo.name"
         :data-photo-index="idx"
         loading="lazy"
-        w-full aspect-square object-cover
+        class="w-full h-auto object-contain rounded-xl shadow-md"
       >
     </div>
   </div>

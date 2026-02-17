4b9325e357844118ec72b49a77695f967c3c2b7d
moersou <zhaoyijia2018@gmail.com>
2026-02-17 13:24:56 +0800

    feat: add pics

---
+++ src/components/photos/Photo202601.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Photo } from '../../../photos/data'
+import raw from '../../../photos/data'
+
+const photos: Photo[] = [
+  'p-2026-01-31-17-10-18-000-S',
+  'p-2026-01-31-11-40-56-000-S',
+  'p-2026-01-31-11-34-48-000-S',
+  'p-2026-01-31-11-40-21-000-S',
+].map(name => raw.find(p => p.url.includes(name))!)
+</script>
+
+<template>
+  <PhotoSlide :photos="photos" />
+</template>
